eb9c79fea7dc246ed446103977532361ef407640
pythogaras <106314530+pythogaras@users.noreply.github.com>
2022-05-26 13:57:51 +0200

    Delete week2_2.py

--- week2_2.py
+++
@@ -1,9 +0,0 @@
-name = []
-
-while True:
-    name = name + list([input("What is your name?: ")])
-    print(name)
-    print (str("Btw you can end the script by pressing ctrl+c"))
-                 
-
-          
