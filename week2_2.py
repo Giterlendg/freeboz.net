c8849556e5f75d16e1eee6fb70b037247677a379
pythogaras <106314530+pythogaras@users.noreply.github.com>
2022-05-26 13:48:11 +0200

    Add files via upload

---
+++ week2_2.py
@@ -0,0 +1,9 @@
+name = []
+
+while True:
+    name = name + list([input("What is your name?: ")])
+    print(name)
+    print (str("Btw you can end the script by pressing ctrl+c"))
+                 
+
+          
